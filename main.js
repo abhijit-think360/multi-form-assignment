4fbd8c811ea625665aae8e51dca37580b1118073
Abhijit Apotikar <abhijit.apotikar@think360.ai>
2022-09-06 14:38:30 +0530

    add one element to html using js

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+var myContainer = document.getElementById('container');
+const companyName = document.createElement("span");
+const node = document.createTextNode("Think360");
+companyName.appendChild(node);
+// myNavBar.appendChild(companyName);
+myContainer.appendChild(companyName);
+//const navBar = document.createElement("nav");
+// myContainer.appendChild(navBar);
+// navBar.setAttribute('id', 'myNavBar');
+
+// const logInBtn = document.createElement("span");
+// const logInBtnNode = document.createTextNode("Log In");
+// logInBtn.appendChild(logInBtnNode);
+// const myNavBar = document.getElementById('myNavBar');
+
+
+// myContainer.appendChild(navBar);
