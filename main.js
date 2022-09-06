55946007735cd18132ac135f079a067a259e9b5c
Abhijit Apotikar <abhijit.apotikar@think360.ai>
2022-09-06 16:39:10 +0530

    save personal form data to local storage

--- main.js
+++ main.js
@@ -30,84 +30,6 @@ myNavBar.appendChild(logInBtn);
 myContainer.appendChild(myNavBar);
 
 
-//const navBar = document.createElement("nav");
-// myContainer.appendChild(navBar);
-// navBar.setAttribute('id', 'myNavBar');
-
-// const logInBtn = document.createElement("span");
-// const logInBtnNode = document.createTextNode("Log In");
-// logInBtn.appendChild(logInBtnNode);
-// const myNavBar = document.getElementById('myNavBar');
-
-
-// myContainer.appendChild(navBar);
-// Create a form dynamically
-// var form = document.createElement("form");
-// form.setAttribute("method", "post");
-// form.setAttribute("action", "submit.php");
-
-// // Create an input element for Full Name
-// var FN = document.createElement("input");
-// FN.setAttribute("type", "text");
-// FN.setAttribute("name", "FullName");
-// FN.setAttribute("placeholder", "Full Name");
-
-//  // Create an input element for date of birth
-//  var DOB = document.createElement("input");
-//  DOB.setAttribute("type", "text");
-//  DOB.setAttribute("name", "dob");
-//  DOB.setAttribute("placeholder", "DOB");
-
-//  // Create an input element for emailID
-//  var EID = document.createElement("input");
-//  EID.setAttribute("type", "text");
-//  EID.setAttribute("name", "emailID");
-//  EID.setAttribute("placeholder", "E-Mail ID");
-
-//   // Create an input element for password
-//   var PWD = document.createElement("input");
-//   PWD.setAttribute("type", "password");
-//   PWD.setAttribute("name", "password");
-//   PWD.setAttribute("placeholder", "Password");
-
-//    // Create an input element for retype-password
-//    var RPWD = document.createElement("input");
-//    RPWD.setAttribute("type", "password");
-//    RPWD.setAttribute("name", "reTypePassword");
-//    RPWD.setAttribute("placeholder", "ReEnter Password");
-
-//             // create a submit button
-//             var s = document.createElement("input");
-//             s.setAttribute("type", "submit");
-//             s.setAttribute("value", "Submit");
-             
-//             // Append the full name input to the form
-//             form.appendChild(FN);
-             
-//             // Inserting a line break
-//             form.appendChild(br.cloneNode());
-             
-//             // Append the DOB to the form
-//             form.appendChild(DOB);
-//             form.appendChild(br.cloneNode());
-             
-//             // Append the emailID to the form
-//             form.appendChild(EID);
-//             form.appendChild(br.cloneNode());
-             
-//             // Append the Password to the form
-//             form.appendChild(PWD);
-//             form.appendChild(br.cloneNode());
-             
-//             // Append the ReEnterPassword to the form
-//             form.appendChild(RPWD);
-//             form.appendChild(br.cloneNode());
-             
-//             // Append the submit button to the form
-//             form.appendChild(s);
-
-//             document.getElementsByTagName("body")[0]
-//             .appendChild(form)
 
 var br = document.createElement("br");
 
@@ -118,21 +40,25 @@ myForm.setAttribute('method', 'post');
 var FN = document.createElement("input");
 FN.setAttribute("type", "text");
 FN.setAttribute("name", "FullName");
+FN.setAttribute('id', 'fullName');
 FN.setAttribute("placeholder", "Full Name");
 
 var DOB = document.createElement("input");
 DOB.setAttribute("type", "text");
 DOB.setAttribute("name", "dob");
+DOB.setAttribute('id', 'dob');
 DOB.setAttribute("placeholder", "DOB");
 
 var EID = document.createElement("input");
 EID.setAttribute("type", "text");
 EID.setAttribute("name", "emailID");
+EID.setAttribute('id', 'email');
 EID.setAttribute("placeholder", "E-Mail ID");
 
 var PWD = document.createElement("input");
 PWD.setAttribute("type", "password");
 PWD.setAttribute("name", "password");
+PWD.setAttribute('id', 'password');
 PWD.setAttribute("placeholder", "Password");
 
 // Create an input element for retype-password
@@ -145,6 +71,10 @@ RPWD.setAttribute("placeholder", "ReEnter Password");
 var s = document.createElement("input");
 s.setAttribute("type", "submit");
 s.setAttribute("value", "Submit");
+s.addEventListener("click", store);
+
+
+
 
 
 myForm.appendChild(FN);
@@ -159,6 +89,44 @@ myForm.appendChild(RPWD);
 myForm.appendChild(br.cloneNode());
 myForm.appendChild(s);
 myForm.appendChild(br.cloneNode());
+
+function store(){
+    var FN = document.getElementById('fullName');
+    var EID= document.getElementById("email");
+    var DOB = document.getElementById("dob");
+    var PWD = document.getElementById("password");
+
+
+    localStorage.setItem("FullName", FN.value);
+    localStorage.setItem("Email", EID.value);
+    localStorage.setItem("DOB", DOB.value);
+    localStorage.setItem("Password", PWD.value);
+    alert("Data saved successfully!:)");
+    const registeredName = document.createElement('p');
+const registeredEmail = document.createElement('p');
+const registeredDOB = document.createElement('p');
+const registeredPassword = document.createElement('p');
+
+var storedName = localStorage.getItem("FullName");
+var storedEmail = localStorage.getItem("Email");
+var storedDOB = localStorage.getItem("DOB");
+var storedPassword = localStorage.getItem("Password");
+
+registeredName.innerHTML = storedName;
+registeredEmail.innerHTML = storedEmail;
+registeredDOB.innerHTML = storedDOB;
+registeredPassword.innerHTML = storedPassword;
+    myContainer.appendChild(registeredName);
+    myContainer.appendChild(registeredEmail);
+    myContainer.appendChild(registeredDOB);
+    myContainer.appendChild(registeredPassword);
+
+    event.preventDefault();
+   }
+
+
+
+
 
 
 myContainer.appendChild(myForm);
